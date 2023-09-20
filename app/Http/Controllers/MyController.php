c27f91461939956a11bcc52f8bc9d248ccd6bdaf
momermohsin <momer14.mohsin@gmail.com>
2023-09-20 17:29:53 +0500

    wow

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -8,30 +8,15 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    public function calculate(Request $request)
-    {
-        $number1 = $request->input('number1');
-        $number2 = $request->input('number2');
-        $operator = $request->input('operator');
-    
-        if ($operator === '+') {
-            $result = $number1 + $number2;
-        } elseif ($operator === '-') {
-            $result = $number1 - $number2;
-        } elseif ($operator === '*') {
-            $result = $number1 * $number2;
-        } elseif ($operator === '/') {
-            if ($number2 != 0) {
-                $result = $number1 / $number2;
-            } else {
-                return view('error')->with('message', 'Division by zero is not allowed.');
-            }
-        } else {
-            return view('error')->with('message', 'Invalid operator. Please use +, -, *, or /.');
-        }
-    
-        return view('calculation')->with('result', $result);
+    public function cal(Request $req){
+
+        $t1 = $req->txt1;
+        $t2 = $req->txt2;
+        $result = $t1 + $t2;
+        $_SESSION["res"] = $result;
+        return view ("calculation");
     }
     
+        
 }
 
